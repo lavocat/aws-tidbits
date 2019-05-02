2b1e5f5df264e004d37650b4a4c4f20f6b46949e
Michael Lavocat <mlavocat@imac.local>
2019-05-01 21:14:46 -0500

    Some updates

--- boto_session.py
+++ boto_session.py
@@ -9,7 +9,9 @@ def get_boto_session():
     if os.getenv("AWS_CONTAINER_CREDENTIALS_RELATIVE_URI", None) is None:
         return boto3.Session()  # This is for running local
     uri = "http://169.254.170.2" + os.getenv("AWS_CONTAINER_CREDENTIALS_RELATIVE_URI")
-    aws = requests.get(uri).json
-    return boto3.Session(aws_access_key_id=aws['AwsAccessKey'],
+    aws = requests.get(uri).json()
+    region = requests.get("http://169.254.169.254/latest/meta-data/placement/availability-zone").text[:-1]
+    return boto3.Session(aws_access_key_id=aws['AccessKeyId'],
                          aws_secret_access_key=aws['SecretAccessKey'],
-                         aws_session_token=aws['Token'])
+                         aws_session_token=aws['Token'],
+                         region_name=region)
--- discover_hosts.py
+++ discover_hosts.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
 
 import boto3
-from aws-tidbits import get_boto_session
+from boto_session import get_boto_session
 
 
-def get_instances_by_tag():
+def get_instances():
     session = get_boto_session()
     ec2 = session.client('ec2')
-    filters = [{  # filter for running instances with specific tag
-        'Name': 'instance-state-name', 'Values': ['running'],
-        'Name': 'tag:Application', 'Values': ['application_name'],
-        }]
+    filters = [
+        # Get running instances
+        {'Name': 'instance-state-name', 'Values': ['running']},
+        ]
     response = ec2.describe_instances(Filters=filters)
     instance_list = []
-    # Get private IP addresses from response
     for reservation in (response["Reservations"]):
         for instance in reservation["Instances"]:
             for network_interface in instance["NetworkInterfaces"]:
                 instance_list.append(instance["PrivateIpAddress"])
-    return instance_list
+    return instance_list  # Returns list object with PrivateIPAddress
